abffb09e9b40b14d568bac3b294853b5ac56d505
Pokora22 <pokora222@gmail.com>
2018-10-30 22:31:44 +0000

    Some checks added to constructors + getters/setters for base classes.

--- src/main/MenuItem.java
+++ src/main/MenuItem.java
@@ -8,4 +8,20 @@ public class MenuItem {
         this.name = name != null && !name.isEmpty()? name: "ITEM NAME WAS NOT GIVEN";
         this.price = price >= 0? ((int)price * 100)/100: 0;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if (name != null) this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        if (price >= 0) this.price = price;
+    }
 }
---
+++ src/test/MenuItemTest.java
@@ -0,0 +1,24 @@
+package test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MenuItemTest {
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void setName() {
+    }
+
+    @Test
+    void getPrice() {
+    }
+
+    @Test
+    void setPrice() {
+    }
+}
