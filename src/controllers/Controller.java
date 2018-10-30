d1495266c5d983a1dbc5028a03eab2ac6981ea16
Pokora22 <pokora222@gmail.com>
2018-10-30 18:45:03 +0000

    Controller master implemented.

--- src/controllers/Controller.java
+++ src/controllers/Controller.java
@@ -1,8 +1,11 @@
 package controllers;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 
@@ -11,11 +14,21 @@ public class Controller {
     private Scene sourceScene;
 
     public void setSourceScene(Scene sourceScene) {
-        this.sourceScene = sourceScene;
+        if (sourceScene != null) {
+            this.sourceScene = sourceScene;
+        }
+        else{
+            Pane pane = new Pane();
+            Label labelGoofed = new Label("Done goofed. This should point to previous scene");
+            pane.getChildren().add(labelGoofed);
+            this.sourceScene = new Scene(pane);
+        }
     }
 
-    public void switchToSourceScene(ActionEvent actionEvent){
+    @FXML
+    private void switchToSourceScene(ActionEvent actionEvent){
         Stage sourceStage = (Stage) ((Node)actionEvent.getSource()).getScene().getWindow();
+
         sourceStage.setTitle("Restaurant Management");
         sourceStage.setScene(sourceScene);
     }
